0bfed9ec177942f69424f554bb3fce107d575dfb
Edudzi Worlasi Osborn <oeworlasi@gmail.com>
2024-06-05 18:46:10 +0000

    Plant UI app completed successfully

---
+++ plant_app_ui/lib/Screen/detail_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app_ui/Screen/product_detail.dart';
+
+class DetailScreen1 extends StatelessWidget {
+  const DetailScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Hero(
+      tag: "flower1",
+      child: ProductDetail(
+        image: "assets/images/flower1.png",
+        name: "House Shape \n Close Plant",
+        price: "\$45",
+      ),
+    );
+  }
+}
+
+class DetailScreen2 extends StatelessWidget {
+  const DetailScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Hero(
+      tag: "flower2",
+      child: ProductDetail(
+        image: "images/flower2.png",
+        name: "Glass Water",
+        price: "\$60",
+      ),
+    );
+  }
+}
